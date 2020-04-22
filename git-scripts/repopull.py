c93097ff425082c91eb46593d992e7236d658942
Xinchao Song <git@xinchaosong.com>
2020-04-22 08:27:03 -0400

    Update git scripts

--- git-scripts/repopull.py
+++ git-scripts/repopull.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 
+# Created by Xinchao Song, contact@xinchaosong.com.
+# April 21, 2020
+# Copyright © 2020 Xinchao Song. All rights reserved.
+
 import sys
 import os
 import subprocess
--- git-scripts/repopull.sh
+++ git-scripts/repopull.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+# Created by Xinchao Song, contact@xinchaosong.com.
+# April 21, 2020
+# Copyright © 2020 Xinchao Song. All rights reserved.
+
 if [ -n "$1" ]; then
-    python3 repopull.py "$1"
+  python3 repopull.py "$1"
 else
-    python3 repopull.py
+  python3 repopull.py
 fi
