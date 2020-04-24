20248b9d7a2e78bba8c2a789938e3244e88e8087
Xinchao Song <git@xinchaosong.com>
2020-04-24 03:57:25 -0400

    Re-add the feature of saving grades to csv

--- grading-scripts/grade.py
+++ grading-scripts/grade.py
@@ -33,6 +33,15 @@ def load_csv(csv_path):
     return data_sheet
 
 
+def save_grades(csv_path, roster):
+    headers = list(roster[next(iter(roster))].keys())
+
+    with open(csv_path, 'w') as f:
+        writer = csv.DictWriter(f, headers)
+        writer.writeheader()
+        writer.writerows(roster.values())
+
+
 def generate_compiling_cnd(config):
     gcc_cmd = config['command']
     source_files = config['source_files']
@@ -47,7 +56,7 @@ def generate_compiling_cnd(config):
     return grader_gcc_cmd, student_gcc_cmd
 
 
-def grade_single(config, student_info, test_list, run_test, check_leak, show_details=True, to_csv=False):
+def grade_single(config, student_info, test_list, run_test, check_leak, show_details=True):
     assignment_title = config['assignment_title']
     grader_gcc_cmd, student_gcc_cmd = generate_compiling_cnd(config)
     last_name = student_info['last_name'].lower()
@@ -55,33 +64,35 @@ def grade_single(config, student_info, test_list, run_test, check_leak, show_det
     repo_name = student_info['repo_name']
     folder_name = last_name + '_' + first_name + '/' + repo_name
     assignment_path = folder_name + '/' + assignment_title
+    total_grade = 0
     grading_comment = ""
+    all_grades = {i_tid: 0 for i_tid in test_list}
+    all_grades['total'] = 0
 
     print(first_name + " " + last_name + ":")
 
     if run_test:
-        total = 0
         score_deducted = 0
         command = None
 
         try:
-            command = "cp %s %s/%s" \
+            command = "cp %s %s/%s " \
                       "&& cd %s " \
                       "&& %s " \
                       % (config['grader_test_file'], assignment_path, config['grader_test_file'],
                          assignment_path,
                          grader_gcc_cmd)
-            subprocess.call(command, shell=True)
+            subprocess.check_output(command, shell=True)
 
         except subprocess.CalledProcessError:
-            print("cp fails: %s" % command)
-            return
+            print("cp fails: %s\n" % command)
+            return all_grades
 
-        for tid, item in test_list.items():
+        for i_tid, i_value in test_list.items():
             try:
                 command = "./%s/%s" % (assignment_path, config['grader_target'])
 
-                score_output = subprocess.check_output([command, tid], shell=False, timeout=30)
+                score_output = subprocess.check_output([command, i_tid], shell=False, timeout=30)
                 score_output = score_output.decode().splitlines()
 
                 if score_output[-1].startswith("Score:"):
@@ -94,17 +105,18 @@ def grade_single(config, student_info, test_list, run_test, check_leak, show_det
                 score = 0
 
             if score == 0:
-                score_deducted -= item['points']
-                grading_comment += "(-" + item['points'] + ") " + item['grading_comment'] + "\n"
+                score_deducted -= float(i_value['points'])
+                grading_comment += "(-" + i_value['points'] + ") " + i_value['grading_comment'] + "\n"
 
-            total += score
+            total_grade += score
+            all_grades[i_tid] = score
 
             if show_details:
-                print("Test case #%s: %f" % (tid, score))
+                print("Test case #%s: %f" % (i_tid, score))
 
+        all_grades['total'] = total_grade
         full_points = sum(float(test_list[i]['points']) for i in test_list)
-
-        print("Lost points: %.2f, total grade: %.2f / %.2f\n" % (score_deducted, total, full_points))
+        print("Lost points: %.2f, total grade: %.2f / %.2f\n" % (score_deducted, total_grade, full_points))
 
         if show_details:
             print("Grading comments:")
@@ -135,11 +147,9 @@ def grade_single(config, student_info, test_list, run_test, check_leak, show_det
         if show_details:
             print("\n")
 
-        for idx in range(len(config['memory_leak_test_id'])):
-
+        for i_tid in config['memory_leak_test_id']:
             if show_details:
-                print("Memory leak check: grading test case #%d\n" %
-                      config['memory_leak_test_id'][idx])
+                print("Memory leak check: grading test case #%d\n" % i_tid)
 
             try:
                 command = "cd %s " \
@@ -147,8 +157,7 @@ def grade_single(config, student_info, test_list, run_test, check_leak, show_det
                 subprocess.call(command, shell=True)
 
                 exe_path = "./%s/%s" % (assignment_path, config['grader_target'])
-                test_id = str(config['memory_leak_test_id'][idx])
-                subprocess.call(["valgrind", "--log-fd=1", exe_path, test_id], shell=False, timeout=30)
+                subprocess.call(["valgrind", "--log-fd=1", exe_path, str(i_tid)], shell=False, timeout=30)
 
             except subprocess.CalledProcessError:
                 print("Memory leak examination: ERROR\n")
@@ -156,6 +165,8 @@ def grade_single(config, student_info, test_list, run_test, check_leak, show_det
 
             if show_details:
                 print("\n")
+
+    return all_grades
 
 
 def grade(sid, run_test, check_leak):
@@ -167,8 +178,12 @@ def grade(sid, run_test, check_leak):
 
         if sid is None:
             for i_student in roster.values():
-                grade_single(config=i_config, student_info=i_student, test_list=test_list,
-                             run_test=run_test, check_leak=check_leak, show_details=False)
+                all_grades = grade_single(config=i_config, student_info=i_student, test_list=test_list,
+                                          run_test=run_test, check_leak=False, show_details=False)
+                i_student.update(all_grades)
+
+            save_grades(i_config['assignment_title'] + "_grades.csv", roster=roster)
+
         else:
             grade_single(config=i_config, student_info=roster[sid], test_list=test_list,
                          run_test=run_test, check_leak=check_leak, show_details=True)
